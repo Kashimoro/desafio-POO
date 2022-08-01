ca27312f01bd3d6ffebed787a94b22daa8174374
Yuri Ferreira <ynogueiira@gmail.com>
2022-07-31 21:41:50 -0300

    Criando classes curso e mentoria e instanciando e exibindo os objetos

--- desafio-POO/src/dio/Application.java
+++ desafio-POO/src/dio/Application.java
@@ -1,10 +1,33 @@
 package dio;
 
+import java.time.LocalDate;
+
+import dio.dominio.Curso;
+import dio.dominio.Mentoria;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		Curso curso1 = new Curso();
+		
+		curso1.setTitulo("Curso java");
+		curso1.setDescricao("descricao curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		
+		curso2.setTitulo("Curso poo");
+		curso2.setDescricao("descricao curso poo");
+		curso2.setCargaHoraria(8);
 
+		Mentoria mentoria1 = new Mentoria();
+		mentoria1.setTitulo("mentoria de java");
+		mentoria1.setDescricao("descricao mentoria java");
+		mentoria1.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria1);
 	}
 
 }
